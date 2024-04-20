6ccd9ccb06c272ee217173d337b50ee4cbc11bad
Mmy2000 <yahianada005@gmail.com>
2024-04-20 16:24:56 +0200

    starting with js

--- js/main.js
+++ js/main.js
@@ -44,3 +44,33 @@ $('.side a').click(function(e){
     let sectionOffset = $(linkHref).offset().top
     $('body').animate({scrollTop:sectionOffset},500)
 })
+
+window.onload = function() {
+   
+    countDownToTime("21 april 2024 10:00:00");
+  }
+
+  function countDownToTime(countTo) {
+  
+        let futureDate = new Date(countTo);
+    futureDate = (futureDate.getTime()/1000);
+
+    let now = new Date();
+    now = (now.getTime()/1000);
+
+    timeDifference = (futureDate- now);
+        
+   let days = Math.floor( timeDifference / (24*60*60));
+   let hours = Math.floor((timeDifference - (days * (24*60*60))) / 3600);
+   let mins = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600 )) / 60);
+   let secs = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600) - (mins * 60)))
+
+
+    $(".days").html(`${days} D`);
+    $(".hours").html(`${hours} h`);
+    $(".minutes").html(`${ mins } m`);
+    $('.seconds').html(`${ secs} s`)
+
+  
+    setInterval(function() {  countDownToTime(countTo); }, 1000);
+  }
